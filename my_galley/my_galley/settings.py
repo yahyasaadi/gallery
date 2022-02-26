c08f2bae916baf20b86b337268cd611ee80da5ac
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-26 12:13:30 +0300

    added url

--- my_galley/my_galley/settings.py
+++ my_galley/my_galley/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
