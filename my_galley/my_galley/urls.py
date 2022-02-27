1fad6aa6dc5c159a8a3cb585145d9dd005163102
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-27 11:36:03 +0300

    import static for images to load properly

--- my_galley/my_galley/urls.py
+++ my_galley/my_galley/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# If static file not working add the next 2 lines
+from django.conf import settings
+from django.conf.urls.static import static
 from photos import views as photo_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('photos.urls'))
 ]
+
+# And this line
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
