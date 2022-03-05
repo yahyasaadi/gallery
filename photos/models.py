3add1a83a05c078119b965881e6ff03c0bfa8e77
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-05 13:54:13 +0300

    Added a search functionality

--- photos/models.py
+++ photos/models.py
@@ -40,7 +40,7 @@ class Category(models.Model):
     def __str__(self):
         return f'{self.img_category}'
 
-
+# 
 class Photo(models.Model):
     img = models.ImageField(upload_to='images')
     img_name = models.CharField(max_length=50)
--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Photo, Location, Category
 
 # Create your views here.
@@ -11,13 +12,10 @@ def home(request):
 
 # Search method
 def search(request):
-    if 'category' in request.GET and request.GET["category"]:
-        search_term = request.GET.get("category")
-        searched_images = Photo.search_image(search_term)
-        message = f"{search_term}"
-
-        return render(request, 'photos/search.html',{"message":message,"req_images": searched_images})
-
-    else:
-        message = "You haven't searched for any term"
-        return render(request, 'photos/search.html',{"message":message})
+	if request.method == "POST":
+		searched = request.POST['searched']
+		pics = Photo.objects.filter(img_category__img_category__contains=searched)
+	
+		return render(request, 'photos/search.html', {'searched':searched, 'pics':pics})
+	else:
+		return render(request, 'photos/search.html')
