15ad1aef5b97ffb73f8004362076551ccb572718
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-06 22:30:39 +0300

    Updated settings

--- my_galley/settings.py
+++ my_galley/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_DJ')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
 
 ALLOWED_HOSTS = ['noorsgallery.herokuapp.com', '127.0.0.1']
 
